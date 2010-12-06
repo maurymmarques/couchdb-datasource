68e0fce2e2fe33a508dd7901239b367b77bfd2e4
Maury <maurymmarques@gmail.com>
2010-12-06 01:12:26 -0200

    Alteração do path do cakephp framework e limpeza de cache

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -50,7 +50,7 @@
  *
  */
 	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-		define('CAKE_CORE_INCLUDE_PATH', 'C:' . DS . 'xampp' . DS . 'htdocs' . DS . 'cake_1.3.3');
+		define('CAKE_CORE_INCLUDE_PATH', ROOT);
 	}
 
 /**

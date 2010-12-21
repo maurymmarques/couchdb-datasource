75c374ce6c53a80ecde827ded7c8a0982c826893
Maury M. Marques <maurymmarques@gmail.com>
2010-12-20 22:28:36 -0200

    Define 'Content-Type' = 'application/json';

--- app/models/datasources/couchdb_source.php
+++ app/models/datasources/couchdb_source.php
@@ -8,16 +8,16 @@
  * CAKEPHP Version 1.3.x
  *
  * Reference:
- * 	  gwoo couchsource datasource (http://bin.cakephp.org/view/925615535#modify)
- * 	  Trabalhando com o couchdb (http://www.botecounix.com.br/blog/?p=1375) 
+ * gwoo couchsource datasource (http://bin.cakephp.org/view/925615535#modify)
+ * Trabalhando com o couchdb (http://www.botecounix.com.br/blog/?p=1375)
  *
- * Copyright 2010, Maury M. Marques http://github.com/maurymmarques/ 
+ * Copyright 2010, Maury M. Marques http://github.com/maurymmarques/
  *
  * Disponibilizado sob licença MIT.
  * Redistribuições dos arquivos devem manter a nota de copyright acima.
- * 
+ *
  * @package couchdb
- * @subpackage couchdb.models.datasources 
+ * @subpackage couchdb.models.datasources
  * @filesource
  * @copyright Copyright 2010, Maury M. Marques http://github.com/maurymmarques/
  * @license http://www.opensource.org/licenses/mit-license.php A licença MIT
@@ -37,6 +37,7 @@ class CouchdbSource extends DataSource{
 	public function __construct($config = null, $autoConnect = true){
 		if(!isset($config['request'])){
 			$config['request']['uri'] = $config;
+			$config['request']['header']['Content-Type'] = 'application/json';
 		}
 		parent::__construct($config);
 		$this->fullDebug = Configure::read() > 1;
@@ -87,8 +88,9 @@ class CouchdbSource extends DataSource{
 	 * @access public
 	 */
 	public function close(){
-		if(Configure::read() > 1){//$this->showLog();
-}
+		if(Configure::read() > 1){
+			//$this->showLog();
+		}
 		$this->disconnect();
 	}
 
